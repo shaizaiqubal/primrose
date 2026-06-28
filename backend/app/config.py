f8d50d2a751a80a0362024f0712721b8757bf7ed
shaizaiqubal <shaizaiqubal@gmail.com>
2026-06-28 19:50:23 +0530

    chore: add config file and .env.example template

---
+++ backend/app/config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+QDRANT_URL = os.getenv("QDRANT_URL")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+QDRANT_PORT = int(os.getenv("QDRANT_PORT", 6333))
+HF_TOKEN = os.getenv("HF_TOKEN")
